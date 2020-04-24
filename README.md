ddc2070a3caef24b0f1e93ebc6d4e7078c4c6c33
v-rathod <v.s.rathod@gmail.com>
2020-04-24 16:05:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-autocomplete-highlight-q1f7nk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-autocomplete-highlight-q1f7nk)
---
+++ app/autocomplete-overview-example.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
+
+export class State {
+  constructor(public name: string, public population: string, public flag: string) { }
+}
+
+/**
+ * @title Autocomplete overview
+ */
+@Component({
+  selector: 'autocomplete-overview-example',
+  templateUrl: 'autocomplete-overview-example.html',
+  styleUrls: ['autocomplete-overview-example.css']
+})
+export class AutocompleteOverviewExample {
+  stateCtrl: FormControl;
+  filteredStates: Observable<any[]>;
+  toHighlight: string = '';
+  keyed: boolean;
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
+    }
+  ];
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => this.filterStates(state))
+      );
+  }
+
+
+  filterStates(name: string) {
+    this.toHighlight = name;
+    return this.states.filter(state =>
+      state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+}
+
+
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Pipe({ name: 'highlight' })
+export class HighlightPipe implements PipeTransform {
+  transform(text: string, search): string {
+    const pattern = search
+      .replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
+      .split(' ')
+      .filter(t => t.length > 0)
+      .join('|');
+    const regex = new RegExp(pattern, 'gi');
+    const match = text.match(regex);
+    if (!match) {
+      return text;
+    }
+    return search ? text.replace(regex, match => `<b>${match}</b>`) : text;
+  }
+}
